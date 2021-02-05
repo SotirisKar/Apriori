0197c106b03f4d9be481b78d689a1b87e5555351
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2021-02-05 17:44:31 +0200

    Add files via upload

---
+++ apriori.py
@@ -0,0 +1,12 @@
+from efficient_apriori import apriori
+transactions = [('onion', 'potato', 'bread','burger'),
+                ('onion','potato','burger','coffee'),
+                ('onion','potato','coffee')]
+
+
+itemsets, rules = apriori(transactions ,min_support=0, min_confidence=0)
+
+
+rules_rhs = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 2, rules)
+for rule in sorted(rules_rhs, key=lambda itemsets: transactions):
+  print(rule)
