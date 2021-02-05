1a105f6f13a0f51a2ee4bfc183dfe1e57b7b4d40
Sotiris <36128807+SotirisKar@users.noreply.github.com>
2021-02-05 17:44:59 +0200

    Update apriori.py

--- apriori.py
+++ apriori.py
@@ -7,6 +7,6 @@ transactions = [('onion', 'potato', 'bread','burger'),
 itemsets, rules = apriori(transactions ,min_support=0, min_confidence=0)
 
 
-rules_rhs = filter(lambda rule: len(rule.lhs) == 2 and len(rule.rhs) == 2, rules)
+rules_rhs = filter(lambda rule: len(rule.lhs) == 1 and len(rule.rhs) == 1, rules)
 for rule in sorted(rules_rhs, key=lambda itemsets: transactions):
   print(rule)
